5c6f65ba4b39b36ff1bd909c604d78c61afccb0f
manuella191 <livinha58@gmail.com>
2016-05-24 16:22:17 -0300

    view - 2
    Reestruturação da view- Manuella

---
+++ DMV/view 2/proposta.php
@@ -0,0 +1,61 @@
+<?php
+$titulo = 'Envio de proposta de intercâmbio';
+require 'header.php';
+	
+require 'nav.php';
+
+?>
+<main>
+
+	<h1>Envio de proposta para programa de intercâmbio</h1>
+	
+	<form method="POST"
+		action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>">
+		<fieldset>
+			<legend>Dados do Curso</legend>
+	
+			<p>
+				<label>Nome da escola/Agente:<input type="text" name="escolaagente"></label>
+			</p>
+	
+			<p>
+				<label>País: <input type="text" name="pais"></label>
+			</p>
+	
+			<p>
+				<label>Cidade: <input type="text" name="cidadeescola"></label>
+			</p>
+	
+			<p>
+				<label>Tipo de curso <input type="text" name="tipocuros"></label>
+			</p>
+	
+			<p>
+				<label>Data de Inicio: <input type="date" name="datainicio"></label>
+			</p>
+	
+			<p>
+				<label>Data de Termino: <input type="date" name="datatermino"></label>
+			</p>
+	
+			<p>
+				<label>Duração: <input type="text" name="duracao"></label>
+			</p>
+	
+			<p>
+				<label>Material didático: <input type="radio" name="materialdidatico"
+					value="incluso"> <span>Incluso</span> <input type="radio"
+					name="materialdidatico" value="naoincluso"> <span>Não Incluso</span>
+				</label>
+			</p>
+	
+		</fieldset>
+	
+		<input type="submit" name="enviar" value="Enviar">
+	
+	</form>
+
+</main>
+<?php
+require_once 'footer.php';
+?>
---
+++ DMV/view 2/usuario.php
@@ -0,0 +1,26 @@
+<?php 
+	$titulo = 'DMV Intercâmbio';
+	require_once 'header.php';
+	
+	require_once 'nav.php';
+?>
+
+			<main>
+					<h1>Area de Usuário</h1>
+					<p>
+					    <ul>
+					        
+						    <li><a href="atualizar.php">Atualizar cadastro</a></li>
+						    <li><a href="processo.php">Acompanhamento de tramite</a></li>
+						    <li><a href="proposta.php">Enviar Proposta de Intercâmbio</a></li>
+						</ul>
+					</p>
+			</main>	
+			
+<?php 
+	require_once ('footer.php');
+?>	
+					
+			
+			  
+				
