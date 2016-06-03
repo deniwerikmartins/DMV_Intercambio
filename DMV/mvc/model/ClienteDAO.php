f9b29742bf1f8778556702b81e5482f73421b537
redblood666 <redblood666@hotmail.com>
2016-06-03 06:39:00 -0300

    session, login e cadastro
    implementação do login, session e ajuste no cadastro de novo cliente

--- DMV/mvc/model/ClienteDAO.php
+++ DMV/mvc/model/ClienteDAO.php
@@ -6,7 +6,8 @@ class ClienteDAO{
         if ($mysqli->connect_errno) {
             return "Falha no MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
         }
-        $stmt = $mysqli->prepare("INSERT INTO Usuario(cd_User,nm_NomeCliente, ds_Endereco, ds_Complemento, 
+        
+        $stmt = $mysqli->prepare("INSERT INTO Cliente(cd_Usuario,nm_Cliente, ds_Endereco, ds_Complemento, 
         nm_Bairro, nm_Cidade, nm_Estado, cd_CEP, cd_TelRes, cd_Celular, cd_TelComercial, 
         dt_Nascimento, nm_Nacionalidade, cd_RG, cd_CPF, cd_Passaporte, dt_ValidoAte, nm_Profissao, 
         nm_Email, nm_Mae, cd_CelMae, cd_TelComercialMae, nm_EmailMae, nm_Pai, cd_CelPai, 
@@ -27,20 +28,26 @@ class ClienteDAO{
         }
         
  
-        $stmt = $mysqli->prepare("SELECT cd_Usuario FROM Usuario ORDER BY cd_Usuario DESC LIMIT 1");
+        
+        $stmt = $mysqli->prepare("SELECT cd_Cliente FROM Cliente ORDER BY cd_Cliente DESC LIMIT 1");
         $stmt->execute();
         $stmt->bind_result($usuario);
         $stmt->fetch();
-        //echo $usuario;
         
+        $stmt->close();
         
-        $stmt = $mysqli->prepare("INSERT INTO Pagante (cd_Usuario) VALUES(?)");
+        //var_dump($usuario);
+        //$usuario = 1;
+        
+        /*
+        $stmt = $mysqli->prepare("INSERT INTO Pagante (cd_Cliente) VALUES(?)");
         $stmt->bind_param("i",$usuario);
         if (!$stmt->execute()) {
             return "Erro: (" . $stmt->errno . ") " . $stmt->error . "<br>";
         }
+        */
         
-        
+       
         $stmt = $mysqli->prepare("INSERT INTO Pagante VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?)");
         $stmt->bind_param("issssssiiiisi",$usuario,$c->getNomePagante(),$c->getEnderecoPagante(),$c->getComplementoPagante(),
         $c->getBairroPagante(),$c->getCidadePagante(),$c->getEstadoPagante(),$c->getCepPagante(),$c->getRespPagante(),
@@ -49,15 +56,17 @@ class ClienteDAO{
             return "Erro: (" . $stmt->errno . ") " . $stmt->error . "<br>";
         }
         
+        $stmt->close();
         
-        $stmt = $mysqli->prepare("INSERT INTO Programa_Intercambio (cd_Usuario, nm_Pais, nm_Cidade,
-        ds_TipoCurso, dt_InicioCurso, dt_TerminoCurso, ds_Duracao, ic_MaterialDidatico)
-        VALUES(?,?,?,?,?,?,?,?)");
-        $stmt->bind_param("issssssi",$usuario,$c->getEscolaAgente(),$c->getPais(),$c->getCidadeEscola(),$c->getTipoCurso(),$c->getDataInicio(),
+        $stmt = $mysqli->prepare("INSERT INTO Programa_Intercambio (cd_Cliente, nm_EscolaAgente,
+        nm_Pais, nm_Cidade, ds_TipoCurso, dt_InicioCurso, dt_TerminoCurso, ds_Duracao, ic_MaterialDidatico)
+        VALUES(?,?,?,?,?,?,?,?,?)");
+        $stmt->bind_param("isssssssi",$usuario,$c->getEscolaAgente(),$c->getPais(),$c->getCidadeEscola(),$c->getTipoCurso(),$c->getDataInicio(),
         $c->getDataTermino(),$c->getDuracao(),$c->getMaterialDidatico());
         if (!$stmt->execute()) {
             return "Erro: (" . $stmt->errno . ") " . $stmt->error . "<br>";
         }
+        
         
         $stmt->close();
         //return "";
@@ -66,10 +75,10 @@ class ClienteDAO{
 
 public function getCadastro($id){
         $mysqli = new mysqli("127.0.0.1", "root", "", "dmv");
-        $stmt = $mysqli->prepare("SELECT * FROM Usuario WHERE id=?");
+        $stmt = $mysqli->prepare("SELECT * FROM Cliente WHERE id=?");
         $stmt->bind_param("i",$id);
         $stmt->execute();
-        $stmt->bind_result($id, $nm_Usuario, $ds_Endereco, $ds_Complemento, 
+        $stmt->bind_result($id, $nm_Cliente, $ds_Endereco, $ds_Complemento, 
         $nm_Bairro, $nm_Cidade, $nm_Estado, $cd_CEP, $cd_TelRes, $cd_Celular, $cd_TelComercial, 
         $dt_Nascimento, $nm_Nacionalidade, $cd_RG, $cd_CPF, $cd_Passaporte, $dt_ValidoAte, $nm_Profissao, 
         $nm_Email, $nm_Mae, $cd_CelMae, $cd_TelComercialMae, $nm_EmailMae, $nm_Pai, $cd_CelPai, 
@@ -77,7 +86,7 @@ public function getCadastro($id){
         $cd_TelComercialEmergencia, $nm_EmailEmergencia, $sg_NivelConhecimento, $ic_Fumante, 
         $ic_Vegetariano, $ic_Alergico, $ds_Alergias, $ds_OBS);
         $stmt->fetch();
-        $cad = new Cadastro($id, $nm_Usuario, $ds_Endereco, $ds_Complemento, 
+        $cad = new Cadastro($id, $nm_Cliente, $ds_Endereco, $ds_Complemento, 
         $nm_Bairro, $nm_Cidade, $nm_Estado, $cd_CEP, $cd_TelRes, $cd_Celular, $cd_TelComercial, 
         $dt_Nascimento, $nm_Nacionalidade, $cd_RG, $cd_CPF, $cd_Passaporte, $dt_ValidoAte, $nm_Profissao, 
         $nm_Email, $nm_Mae, $cd_CelMae, $cd_TelComercialMae, $nm_EmailMae, $nm_Pai, $cd_CelPai, 
@@ -85,6 +94,35 @@ public function getCadastro($id){
         $cd_TelComercialEmergencia, $nm_EmailEmergencia, $sg_NivelConhecimento, $ic_Fumante, 
         $ic_Vegetariano, $ic_Alergico, $ds_Alergias, $ds_OBS);
         return $cad;
+    }
+    
+    
+     public function update(Cliente $c){
+        $mysqli = new mysqli("127.0.0.1", "root", "", "dmv");
+        if ($mysqli->connect_errno) {
+            return "Falha no MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+        }
+        $stmt = $mysqli->prepare("UPDATE Cliente set nm_Cliente = ?, ds_Endereco = ?, ds_Complemento = ?, 
+        nm_Bairro = ?, nm_Cidade = ?, nm_Estado = ?, cd_CEP = ?, cd_TelRes= ?, cd_Celular = ?, cd_TelComercial = ?, 
+        dt_Nascimento = ?, nm_Nacionalidade = ?, cd_RG = ?, cd_CPF = ?, cd_Passaporte = ?, dt_ValidoAte = ?, nm_Profissao = ?, 
+        nm_Email = ?, nm_Mae = ?, cd_CelMae = ?, cd_TelComercialMae = ?, nm_EmailMae = ?, nm_Pai = ?, cd_CelPai = ?, 
+        cd_TelComercialPai = ?, nm_EmailPai = ?, nm_ContatoEmergencia = ?, cd_CelEmergencia = ?, 
+        cd_TelComercialEmergencia = ?, nm_EmailEmergencia = ?, sg_NivelConhecimento = ?, ic_Fumante = ?, 
+        ic_Vegetariano = ?, ic_Alergico = ?, ds_Alergias = ?, ds_OBS = ? where cd_User = ? ) 
+        ");
+        
+        $stmt->bind_param("ssssssiiiissiiissssiissiissiissiiissi",
+        $c->getNome(),$c->getEndereco(),$c->getComplemento(),$c->getBairro(),$c->getCidade(),$c->getEstado(),
+        $c->getCEP(),$c->getResidencial(),$c->getCelular(),$c->getComercial(),$c->getNascimento(),$c->getNacionalidade(),
+        $c->getRg(),$c->getCpf(),$c->getPassaporte(),$c->getValidade(),$c->getProfissao(),$c->getEmail(),$c->getMae(),$c->getCelMae(),
+        $c->getComMae(),$c->getEmailMae(),$c->getPai(),$c->getCelPai(),$c->getComPai(),$c->getEmailPai(),
+        $c->getEmergencia(),$c->getCelEmergencia(),$c->getComEmergencia(),$c->getEmailEmergencia(),$c->getConhecimento(),
+        $c->getFumante(),$c->getVegetariano(),$c->getAlergico(),$c->getAlergias(),$c->getOBS(),$c->getUser());
+        if (!$stmt->execute()) {
+            return "Erro: (" . $stmt->errno . ") " . $stmt->error . "<br>";
+        }
+        $stmt->close();
+        //return "";
     }
 
 }
--- DMV/mvc/model/UsuarioDAO.php
+++ DMV/mvc/model/UsuarioDAO.php
@@ -6,12 +6,12 @@ class UsuarioDAO{
         if ($mysqli->connect_errno) {
             return "Falha no MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
         }
-        $stmt = $mysqli->prepare("INSERT INTO Login( nm_User, cd_Password) VALUES (?,?)");
+        $stmt = $mysqli->prepare("INSERT INTO Usuario(nm_Usuario,nm_Senha) VALUES (?,?)");
         $stmt->bind_param("ss",$u->getNome(),$u->getSenha());
         if (!$stmt->execute()) {
             return "Erro: (" . $stmt->errno . ") " . $stmt->error . "<br>";
         }
-        $stmt = $mysqli->prepare("SELECT * FROM Login order by cd_User DESC LIMIT 1");
+        $stmt = $mysqli->prepare("SELECT * FROM Usuario order by cd_Usuario DESC LIMIT 1");
         $stmt->execute();
         $stmt->bind_result($id,$nome,$senha);
         $stmt->fetch();
@@ -21,8 +21,8 @@ class UsuarioDAO{
     }
     
     public function authUser($login,$senha){
-        $mysqli = new mysqli("127.0.0.1", "redblood666", "", "teste");
-        $stmt = $mysqli->prepare("SELECT id FROM User WHERE login=? AND senha=?");
+        $mysqli = new mysqli("127.0.0.1", "redblood666", "", "dmv");
+        $stmt = $mysqli->prepare("SELECT cd_Usuario FROM Usuario WHERE nm_Usuario=? AND nm_Senha=?");
         $stmt->bind_param("ss",$login,$senha);
         $stmt->execute();
         $stmt->bind_result($id);
@@ -42,7 +42,7 @@ class UsuarioDAO{
         if ($mysqli->connect_errno) {
             return "Falha no MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
         }
-        $stmt = $mysqli->prepare("INSERT INTO Tramite(cd_Usuario) VALUES (?)");
+        $stmt = $mysqli->prepare("INSERT INTO Tramite(cd_Cliente) VALUES (?)");
         $stmt->bind_param("i",$i);
         $stmt->execute();                       
         $stmt->close();
