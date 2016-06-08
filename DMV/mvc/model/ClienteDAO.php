5f167f7e851c44ef1df98a4f2feb1718702652b7
redblood666 <redblood666@hotmail.com>
2016-06-08 19:12:15 -0300

    remoção de metodo
    remoção de metodo inutil

--- DMV/mvc/model/ClienteDAO.php
+++ DMV/mvc/model/ClienteDAO.php
@@ -74,32 +74,7 @@ class ClienteDAO{
         $stmt->close();
         
     }
-    
 
-
-        public function getCadastro($id){
-            $mysqli = new mysqli("127.0.0.1", "root", "", "dmv");
-            $stmt = $mysqli->prepare("SELECT * FROM Cliente WHERE id=?");
-            $stmt->bind_param("i",$id);
-            $stmt->execute();
-            $stmt->bind_result($id, $nm_Cliente, $ds_Endereco, $ds_Complemento, 
-            $nm_Bairro, $nm_Cidade, $nm_Estado, $cd_CEP, $cd_TelRes, $cd_Celular, $cd_TelComercial, 
-            $dt_Nascimento, $nm_Nacionalidade, $cd_RG, $cd_CPF, $cd_Passaporte, $dt_ValidoAte, $nm_Profissao, 
-            $nm_Email, $nm_Mae, $cd_CelMae, $cd_TelComercialMae, $nm_EmailMae, $nm_Pai, $cd_CelPai, 
-            $cd_TelComercialPai, $nm_EmailPai, $nm_ContatoEmergencia, $cd_CelEmergencia, 
-            $cd_TelComercialEmergencia, $nm_EmailEmergencia, $sg_NivelConhecimento, $ic_Fumante, 
-            $ic_Vegetariano, $ic_Alergico, $ds_Alergias, $ds_OBS);
-            $stmt->fetch();
-            $cad = new Cadastro($id, $nm_Cliente, $ds_Endereco, $ds_Complemento, 
-            $nm_Bairro, $nm_Cidade, $nm_Estado, $cd_CEP, $cd_TelRes, $cd_Celular, $cd_TelComercial, 
-            $dt_Nascimento, $nm_Nacionalidade, $cd_RG, $cd_CPF, $cd_Passaporte, $dt_ValidoAte, $nm_Profissao, 
-            $nm_Email, $nm_Mae, $cd_CelMae, $cd_TelComercialMae, $nm_EmailMae, $nm_Pai, $cd_CelPai, 
-            $cd_TelComercialPai, $nm_EmailPai, $nm_ContatoEmergencia, $cd_CelEmergencia, 
-            $cd_TelComercialEmergencia, $nm_EmailEmergencia, $sg_NivelConhecimento, $ic_Fumante, 
-            $ic_Vegetariano, $ic_Alergico, $ds_Alergias, $ds_OBS);
-            return $cad;
-        }
-    
     
         public function update(Cliente $c){
             $mysqli = new mysqli("127.0.0.1", "root", "", "dmv");
