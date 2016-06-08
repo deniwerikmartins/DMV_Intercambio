808e423e8b09c28d4af03b9bc5b53ac3d598d276
redblood666 <redblood666@hotmail.com>
2016-06-08 18:34:58 -0300

    Pagina de sucesso em atualização
    implementação de pagina de sucesso em atualização de dados

---
+++ DMV/mvc/view/at.sucesso.php
@@ -0,0 +1,30 @@
+<?php 
+	$titulo = 'Atualização realizada';
+	require 'header.php';
+	
+	require 'nav.php';
+?>
+	<main>
+		<h1>Atualização realizada com sucesso</h1>
+	</main>	
+<?php 
+	require 'footer.php';
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
