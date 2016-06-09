051a8b5009993ba3d341e24470a90822a0429479
redblood666 <redblood666@hotmail.com>
2016-06-09 02:53:27 -0300

    UsuarioController
    verificação de senhas digitadas

--- DMV/mvc/controller/UsuarioController.php
+++ DMV/mvc/controller/UsuarioController.php
@@ -4,16 +4,23 @@ class UsuarioController extends Controller{
 
     
     public function insert(){
-        $senha = $_POST["password"];
-        $custo = '08';
-        $salto = 'Cf1f11ePArKlBJomM0F6aJ';
-        $hash = crypt($senha, '$2a$' . $custo . '$' . $salto . '$');
+        $pass1 = trim($_POST["password"]);
+        $pass2 = trim($_POST["password2"]);
         
-        $u = new Usuario(0,$_POST["usuario"],$hash);
-        $uDAO = new UsuarioDAO;
-        $uDAO->insert($u);
-        $this->view->renderizar("cad.sucesso");
-        
+        if(!empty($pass1) && !empty($pass2) && ($pass1 == $pass2)){
+            $senha = $_POST["password"];
+            $custo = '08';
+            $salto = 'Cf1f11ePArKlBJomM0F6aJ';
+            $hash = crypt($senha, '$2a$' . $custo . '$' . $salto . '$');
+            
+            $u = new Usuario(0,$_POST["usuario"],$hash);
+            $uDAO = new UsuarioDAO;
+            $uDAO->insert($u);
+            $this->view->renderizar("cad.sucesso");
+        }
+        else{
+            header("Location: /usuario/registro");
+        }
     }
 
     
